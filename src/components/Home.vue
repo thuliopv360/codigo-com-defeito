14b7d9bab3facdfb3c4217fa9a68d8cbde963dd0
thuliopv360 <69154566+thuliopv360@users.noreply.github.com>
2021-01-19 15:50:30 -0300

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-container fluid>
+        <v-form>
+            <v-file-input label="Selecione as legendas"
+                prepend-icon="mdi-message-text"
+                append-outer-icon="mdi-send"
+                outlined
+                multiple chips v-model="files" 
+                @click:append-outer="processSubtitle"/>
+        </v-form>
+        <div class="pills">
+            <Pill v-for="word in groupedWords" :key="word.name" 
+                :name="word.name" :amount="word.amount"/>
+        </div>
+    </v-container>
+</template>
+
+<script>
+import { ipcRenderer } from 'electron'
+import Pill from './Pill'
+
+export default {
+    components: { Pill },
+    data: function () {
+        return{
+            files: [],
+            groupedWords: [ 
+                { name: 'i', amount: 1234 },
+                { name: 'you', amount: 900 },
+                { name: 'he', amount: 853 },
+                ]
+        }
+    },
+    methods: {
+        processSubtitle() {
+            console.log(this.files)
+
+            ipcRenderer.send('blabla', 'ping')
+            ipcRenderer.on('blabla', (event, resp) => {
+                console.log(resp)
+            })
+        }
+    }
+}
+</script>
+
+<style>
+    .pills {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+</style>
